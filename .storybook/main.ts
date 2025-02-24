4577ecd7f03fb9ab3a69d3501d8d19402b90b47a
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2025-02-24 17:27:55 +0000

    Adds cover docs and script to publish docs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,18 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    '../src/Intro.mdx',
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
     '@storybook/addon-a11y',
-    '@storybook/addon-coverage'
+    '@storybook/addon-coverage',
   ],
   framework: {
     name: '@storybook/react-vite',
